77f13c608d82800f035730854319301e345f8b79
Magomedali Gajiev <mag198421@gmail.com>
2024-07-09 20:33:23 +0300

    Отслеживание формой сталкивания птички со свинкой, остановки

--- AngryBirdsWinFormsApp/MainForm.cs
+++ AngryBirdsWinFormsApp/MainForm.cs
@@ -1,12 +1,25 @@
+using Timer = System.Windows.Forms.Timer;
+
 namespace AngryBirdsWinFormsApp
 {
     public partial class MainForm : Form
     {
         private Bird bird;
         private Pig pig;
+        private Timer timer = new Timer();
         public MainForm()
         {
             InitializeComponent();
+            timer.Interval = 20;
+            timer.Tick += Timer_Tick;
+        }
+
+        private void Timer_Tick(object? sender, EventArgs e)
+        {
+            if (!bird.IsMoveable())
+            {
+                CreateNewBird();
+            }
         }
 
         private void MainForm_Load(object sender, EventArgs e)
@@ -16,16 +29,27 @@ namespace AngryBirdsWinFormsApp
 
         private void MainForm_Shown(object sender, EventArgs e)
         {
-            bird = new Bird(this);
-            bird.Show();
+            CreateNewBird();
 
             pig = new Pig(this);
             pig.Show();
         }
 
+        private void CreateNewBird()
+        {
+            timer.Stop();
+            if (bird != null)
+            {
+                bird.Clear();
+            }
+            bird = new Bird(this);
+            bird.Show();
+        }
+
         private void MainForm_MouseDown(object sender, MouseEventArgs e)
         {
             bird.SetVelocity(e.X, e.Y);
+            timer.Start();
             bird.Start();
         }
     }
